e5725f27a79e42e08c08466ebf1fd0083e585e0d
Suhas Sumukh <70210383+suhasasumukh@users.noreply.github.com>
2024-01-16 22:41:50 +0530

    Create dependencies.py

---
+++ dependencies.py
@@ -0,0 +1,9 @@
+from IPython.display import display, Javascript, Image
+from google.colab.output import eval_js
+from base64 import b64decode, b64encode
+import cv2
+import numpy as np
+import PIL
+import io
+import html
+import time
